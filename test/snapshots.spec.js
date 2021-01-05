24d7ff5ed47f6d14d83356ce8d854cbcbf2bf275
Frédéric Bonnet <fredericbonnet@free.fr>
2021-01-05 21:02:47 +0100

    Add snapshot tests

---
+++ test/snapshots.spec.js
@@ -0,0 +1,37 @@
+import chai from 'chai';
+import fs from 'fs';
+import path from 'path';
+import { renderGraphdown } from '../render.js';
+
+const { expect, Assertion } = chai;
+
+Assertion.addMethod('matchSnapshot', function (filename, snapshot = false) {
+  const filepath = path.join(__dirname, filename);
+  const actual = this._obj;
+  if (snapshot || !fs.existsSync(filepath)) {
+    fs.writeFileSync(filepath, actual);
+  } else {
+    const expected = fs.readFileSync(filepath).toString();
+    expect(actual).to.equal(expected, 'expected data to match snapshot');
+  }
+});
+
+function render(filename) {
+  const data = fs.readFileSync(path.join(__dirname, filename)).toString();
+  const style = fs
+    .readFileSync(path.join(__dirname, '../graphdown.css'))
+    .toString();
+
+  return renderGraphdown(data, style);
+}
+
+describe('snapshots', () => {
+  specify('kitchensink', function () {
+    const gd = render('./examples/kitchensink.txt');
+    expect(gd).to.matchSnapshot('./examples/kitchensink.svg');
+  });
+  specify('sample', function () {
+    const gd = render('./examples/sample.txt');
+    expect(gd).to.matchSnapshot('./examples/sample.svg');
+  });
+});
