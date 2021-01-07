63afb25f3ceb17e37a2161c8b298a580e85514ba
Frédéric Bonnet <fredericbonnet@free.fr>
2021-01-07 19:08:17 +0100

    Add browser exports and licence file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,11 +7,18 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.1]
+
+### Added
+
+- Add browser exports
+
 ## [0.1.0]
 
 ### Added
 
 - First release.
 
-[unreleased]: https://github.com/fredericbonnet/graphdown/compare/v0.1.0...HEAD
+[unreleased]: https://github.com/fredericbonnet/graphdown/compare/v0.1.1...HEAD
+[0.1.1]: https://github.com/fredericbonnet/graphdown/releases/tag/v0.1.1
 [0.1.0]: https://github.com/fredericbonnet/graphdown/releases/tag/v0.1.0
--- scripts/build.js
+++ scripts/build.js
@@ -4,6 +4,7 @@ build({
   entryPoints: ['src/index.js'],
   bundle: true,
   target: 'es2018',
+  platform: 'node',
   format: 'esm',
   outfile: 'dist/index.mjs',
 })
@@ -12,9 +13,24 @@ build({
       entryPoints: ['src/index.js'],
       bundle: true,
       target: 'es2018',
+      platform: 'node',
       format: 'cjs',
       outfile: 'dist/index.cjs',
     })
   )
+  .then(() =>
+    build({
+      entryPoints: ['src/index.js'],
+      bundle: true,
+      target: 'es2018',
+      platform: 'browser',
+      format: 'esm',
+      outfile: 'dist/index.js',
+    })
+  )
   .then(() => copy('css', 'dist/css'))
-  .catch(() => process.exit(1));
+  .then(() => copy('src/index.d.ts', 'dist/index.d.ts'))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
